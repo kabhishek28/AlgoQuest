46d444c635f71b98b0683f33ec774aa4a527c617
kabhishek28 <kabhishek.eng@gmail.com>
2025-08-30 00:31:32 +0530

    Find the sum of the digits of a number.

--- Ninja Question/src/com/xworkz/ninjaquestion/internal/FindSumOfNumber.java
+++ Ninja Question/src/com/xworkz/ninjaquestion/internal/FindSumOfNumber.java
@@ -10,5 +10,4 @@ public class FindSumOfNumber {
         }
         return sum;
     }
-
 }
--- Ninja Question/src/com/xworkz/ninjaquestion/runner/NinjaRunner.java
+++ Ninja Question/src/com/xworkz/ninjaquestion/runner/NinjaRunner.java
@@ -5,6 +5,7 @@ import com.xworkz.ninjaquestion.internal.FindSumOfNumber;
 public class NinjaRunner {
     public static void main(String[] args) {
         FindSumOfNumber findSumOfNumber = new FindSumOfNumber();
+
         System.out.println("Find the sum of the digits of a number. = "+findSumOfNumber.sumOfNumber(111));
     }
 }
